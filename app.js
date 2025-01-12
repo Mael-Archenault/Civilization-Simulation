420970b44ba43ce3d0c7b70e0fd8f0e1059ab2b1
Mael-Archenault <mael.archenault@gmail.com>
2025-01-12 22:29:00 +0100

    value on map generator characteristics

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+
+const express = require('express');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+// const fs = require('fs');
+// const port = 3000;
+
+
+// const server = http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     fs.readFile('index.html', function (err, data) {
+//         if (err) {
+//             res.writeHead(404);
+//             res.write('Error : file not found');
+//         }
+//         else {
+//             res.write(data);
+            
+//         }
+//         res.end();    
+//     });
+// });
+
+// server.listen(port, function (error){
+//     if (error){
+//         console.log("Something went wrong", error);
+//     }
+//     else {
+//         console.log(`Server is running on port ${port}`);
+//     }
+   
+// })
--- map-generator/generationSettings-mg.js
+++ map-generator/generationSettings-mg.js
@@ -5,11 +5,16 @@ const minSize = 5;
 const maxArea = 60; 
 const minArea = 10;
 
-const MGCareaSlider = document.querySelector(".areaSlider");
-const MGCsizeSlider = document.querySelector(".sizeSlider");
-const MGCforestSlider = document.querySelector(".forestSlider");
+const MGCareaSlider = document.querySelector(".area-slider");
+const MGCsizeSlider = document.querySelector(".size-slider");
+const MGCforestSlider = document.querySelector(".forest-slider");
+
+
+const MGCareaValue = document.querySelector(".area-value");
+const MGCsizeValue = document.querySelector(".size-value");
+const MGCforestValue = document.querySelector(".forest-value");
 const MGCseedTextbox = document.querySelector(".seedTextbox");
-const MGCgenerateButton = document.querySelector(".generateButton");
+
 const MGCgenerateRandomButton = document.querySelector(".generateRandomButton");
 
 
@@ -35,5 +40,35 @@ function setRandomMap (){
     setMap();
 }
 
-MGCgenerateButton.onclick = setMap;
 MGCgenerateRandomButton.onclick = setRandomMap;
+
+
+MGCareaSlider.addEventListener('input', (event) => {
+    MGCareaValue.value = event.target.value;
+    setMap();
+});
+
+MGCsizeSlider.addEventListener('input', (event) => {
+    MGCsizeValue.value = event.target.value;
+    setMap();
+});
+
+MGCforestSlider.addEventListener('input', (event) => {
+    MGCforestValue.value = event.target.value;
+    setMap();
+});
+
+MGCareaValue.addEventListener('change', (event) => {
+    MGCareaSlider.value = event.target.value;
+    setMap();
+});
+
+MGCsizeValue.addEventListener('change', (event) => {
+    MGCsizeSlider.value = event.target.value;
+    setMap();
+});
+
+MGCforestValue.addEventListener('change', (event) => {
+    MGCforestSlider.value = event.target.value;
+    setMap();
+});
